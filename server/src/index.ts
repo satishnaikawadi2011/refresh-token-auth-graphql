eaf29de88c61d5f81bc987fa4845d69fc66380b1
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-10-10 00:20:16 +0530

    server and db setup successfully

---
+++ server/src/index.ts
@@ -0,0 +1,79 @@
+// import "reflect-metadata";
+// import {createConnection} from "typeorm";
+// // import {User} from "./entity/User";
+
+// createConnection().then(async () => {
+//     console.log("Connected to Database!!!!");
+
+// }).catch(error => console.log(error));
+import "dotenv/config";
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./UserResolver";
+import { createConnection, getConnection } from "typeorm";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import cors from "cors";
+import { User } from "./entity/User";
+import { sendRefreshToken } from "./sendRefreshToken";
+import { createAccessToken, createRefreshToken } from "./auth";
+
+(async () => {
+  try {
+    await createConnection();
+  } catch (error) {
+    console.log(error);
+  }
+  const app = express();
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true
+    })
+  );
+  app.use(cookieParser());
+  app.get("/", (_req, res) => res.send("hello"));
+  // app.post("/refresh_token", async (req, res) => {
+  //   const token = req.cookies.jid;
+  //   if (!token) {
+  //     return res.send({ ok: false, accessToken: "" });
+  //   }
+
+  //   let payload: any = null;
+  //   try {
+  //     payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+  //   } catch (err) {
+  //     console.log(err);
+  //     return res.send({ ok: false, accessToken: "" });
+  //   }
+
+  //   // token is valid and
+  //   // we can send back an access token
+  //   const user = await User.findOne({ id: payload.userId });
+
+  //   if (!user) {
+  //     return res.send({ ok: false, accessToken: "" });
+  //   }
+
+  //   if (user.tokenVersion !== payload.tokenVersion) {
+  //     return res.send({ ok: false, accessToken: "" });
+  //   }
+
+  //   sendRefreshToken(res, createRefreshToken(user));
+
+  //   return res.send({ ok: true, accessToken: createAccessToken(user) });
+  // });
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [UserResolver]
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+  app.listen(4000, () => {
+    console.log("express server started");
+  });
+})();
